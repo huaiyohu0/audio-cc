23d25512dbe76173641990d967f1eb03d5db9056
zhuhaijun <670518964@qq.com>
2021-06-10 14:48:47 +0800

    初始化

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="container">
-		<audio-cc title="测试音频" playImg="../../static/audio-play.png" pauseImg="../../static/audio-stop.png" src="http://m801.music.126.net/20210610141656/31be8ff937fb43d1495196c7373f1b92/jdymusic/obj/w5zDlMODwrDDiGjCn8Ky/1998233138/a6c9/a022/d609/bc90f27c24742aaaf406f41ad1730516.mp3"></audio-cc>
+		<audio-cc title="测试音频" playImg="../../static/audio-play.png" pauseImg="../../static/audio-stop.png" src="../../static/1.mp3"></audio-cc>
 	</view>
 </template>
 
